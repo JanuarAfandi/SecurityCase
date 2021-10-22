1525d06a05ad1376bf247b3b98e623ba6fce17b8
JanuarAfandi <januarafandi01@gmail.com>
2021-10-22 13:49:04 +0700

    security1

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace securitycase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
